2aa46c8452531ada4524b3631520619abb7d0b55
asmanov <asmanov.a@gmail.com>
2023-07-03 21:25:48 +0300

    Conected with database

---
+++ ViewModel/PharmacyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppPharmacy.Model;
+
+namespace WpfAppPharmacy.ViewModel
+{
+    public class PharmacyDbContext : DbContext
+    {
+        public DbSet<Medicine> medecines { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=PharmacyDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
